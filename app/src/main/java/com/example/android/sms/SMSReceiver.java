b2f43723a2c79e9c783c0adb58c63d9f61750f9a
labnova <progettofeb@gmail.com>
2015-03-26 15:31:31 +0100

    add abortBroadcast() and receiver's priority

---
+++ app/src/main/java/com/example/android/sms/SMSReceiver.java
@@ -0,0 +1,46 @@
+package com.example.android.sms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SMSReceiver extends BroadcastReceiver {
+    public SMSReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+       //prendere l'SMS passato
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs = null;
+        String str = "SMS from ";
+        if(bundle != null) {
+            //ritrovare il messaggio SMS ricevuto
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];
+            for(int i=0; i<msgs.length; i++) {
+                msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                if (i==0) {
+                    //prendi il sender address/phone number
+                    str += msgs[i].getOriginatingAddress();
+                    str += ":";
+                }
+
+                //prendi il corpo del messaggio
+                str += msgs[i].getMessageBody().toString();
+            }
+
+            //visualizza il nuovo messaggio SMS
+            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+            Log.d("SMSReceiver", str);
+
+            //stoppare il broadcasting dell'SMS
+            this.abortBroadcast();
+        }
+
+    }
+}
